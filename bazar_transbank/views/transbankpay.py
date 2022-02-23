d44fefa691970f953492aac5f8b10e47dcb1e521
Cristian Alarcón <53099816+ewigdev@users.noreply.github.com>
2022-02-23 15:40:36 -0300

    correcciones en transbank pay para realización de proceso de compra

--- bazar_transbank/views/transbankpay.py
+++ bazar_transbank/views/transbankpay.py
@@ -5,6 +5,10 @@ import datetime as dt
 from transbank.error.transbank_error import TransbankError
 from transbank.webpay.webpay_plus.transaction import Transaction
 
+from transbank.common.options import WebpayOptions
+from transbank.common.integration_commerce_codes import IntegrationCommerceCodes
+from transbank.common.integration_api_keys import IntegrationApiKeys
+
 import random
 
 def webpay_plus_create(request):
@@ -20,7 +24,8 @@ def webpay_plus_create(request):
     print('return_url: {0}'.format(return_url))
     print('request.headers: {0}'.format(request.headers))
 
-    response = Transaction.create(buy_order, session_id, amount, return_url) 
+    tx = Transaction(WebpayOptions(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY))
+    response = tx.create(buy_order, session_id, amount, return_url)
     print('response: {0}'.format(response))
 
     return render(request, 'send-pay.html', {'response': response, 'amount': amount})    
@@ -29,7 +34,7 @@ def webpay_plus_create(request):
 def commitpay(request):
     print('commitpay')
     print("request: {0}".format(request.POST))    
-    token = request.POST.get('token_ws')
+    token = request.GET.get('token_ws')
 
     TBK_TOKEN = request.POST.get('TBK_TOKEN')
     TBK_ID_SESION = request.POST.get('TBK_ID_SESION')
@@ -39,33 +44,34 @@ def commitpay(request):
     if TBK_TOKEN is None and TBK_ID_SESION is None and TBK_ORDEN_COMPRA is None and token is not None:
 
         #APROBAR TRANSACCIÓN
-        response = Transaction.commit(token=token)
+        tx = Transaction(WebpayOptions(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY))
+        response = tx.commit(token=token)
         print("response: {}".format(response)) 
 
-        status = response.status
+        status = response.get('status')
         print("status: {0}".format(status))
-        response_code = response.response_code
+        response_code = response.get('response_code')
         print("response_code: {0}".format(response_code)) 
         #TRANSACCIÓN APROBADA
         if status == 'AUTHORIZED' and response_code == 0:
 
             state = ''
-            if response.status == 'AUTHORIZED':
+            if response.get('status') == 'AUTHORIZED':
                 state = 'Aceptado'
             pay_type = ''
-            if response.payment_type_code == 'VD':
+            if response.get('payment_type_code') == 'VD':
                 pay_type = 'Tarjeta de Débito'
-            amount = int(response.amount)
+            amount = int(response.get('amount'))
             amount = f'{amount:,.0f}'.replace(',', '.')
-            transaction_date = dt.datetime.strptime(response.transaction_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+            transaction_date = dt.datetime.strptime(response.get('transaction_date'), '%Y-%m-%dT%H:%M:%S.%fZ')
             transaction_date = '{:%d-%m-%Y %H:%M:%S}'.format(transaction_date)
-            transaction_detail = {  'card_number': response.card_detail.card_number,
+            transaction_detail = {  'card_number': response.get('card_detail').get('card_number'),
                                     'transaction_date': transaction_date,
                                     'state': state,
                                     'pay_type': pay_type,
                                     'amount': amount,
-                                    'authorization_code': response.authorization_code,
-                                    'buy_order': response.buy_order, }
+                                    'authorization_code': response.get('authorization_code'),
+                                    'buy_order': response.get('buy_order'), }
             return render(request, 'commit-pay.html', {'transaction_detail': transaction_detail})
         else:
         #TRANSACCIÓN RECHAZADA            
